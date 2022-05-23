d84d3b773c81c00ccc52a6bfe032d2ee828f9ed6
youssef darahem <youssef.darahem@gmail.com>
2022-05-23 14:37:53 +0300

    caera module implemented

--- detectBoard.py
+++ detectBoard.py
@@ -1,4 +1,10 @@
 import cv2 as cv
 def detectBoard(img):
-
-    print('detecting')
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    # Setting parameter values
+    t_lower = 50  # Lower Threshold
+    t_upper = 150  # Upper threshold
+  
+    # Applying the Canny Edge filter
+    edge = cv.Canny(gray, t_lower, t_upper)
+    cv.imshow('edges',edge)
--- main.py
+++ main.py
@@ -1,8 +1,19 @@
+from camera import Camers
 from detectBoard import detectBoard
+
+import cv2 as cv
 
 
 def main():
-    detectBoard()
+    camera = Camers(nano_Camera=False)
+    cap = camera.use()
+    while True:
+        ret, img = cap.read()
+        detectBoard(img=img)
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv.destroyAllWindows()
 
 
 if __name__ == "__main__":
